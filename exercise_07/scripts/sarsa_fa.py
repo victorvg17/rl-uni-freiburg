0174064234e27d03f30daffb58fa41611d450a28
victorvg17 <victorvg17@gmail.com>
2020-04-09 12:51:00 +0200

    function approximation using neural networks

--- exercise_07/scripts/sarsa_fa.py
+++ exercise_07/scripts/sarsa_fa.py
@@ -22,86 +22,76 @@ def tt(ndarray):
   return Variable(torch.from_numpy(ndarray).float(), requires_grad=False)
 
 class Q(nn.Module):
-  def __init__(self, state_dim, action_dim, hidden_dim=50):
-    super(Q, self).__init__()
-    # Implement this
-    self.hidden = nn.Linear(in_features=state_dim, out_features=hidden_dim)
-    #adding relu layer for much needed non-linearity
-    self.relu = nn.ReLU()
-    self.output = nn.Linear(in_features=hidden_dim, out_features=action_dim)
-    # raise NotImplementedError("Architecture of the Q-network is missing.")
+    def __init__(self, state_dim, action_dim, hidden_dim=50):
+        super(Q, self).__init__()
+        # Implement this
+        self.model = nn.Sequential(nn.Linear(in_features=state_dim,
+                                            out_features=hidden_dim),
+                                  nn.ReLU(),
+                                  nn.Linear(in_features=hidden_dim,
+                                            out_features=hidden_dim),
+                                  nn.ReLU(),
+                                  nn.Linear(in_features=hidden_dim,
+                                            out_features=action_dim)
+                                  )
+        # raise NotImplementedError("Architecture of the Q-network is missing.")
 
-  def forward(self, x):
-    # Implement this
-    x = self.hidden(x)
-    x = self.relu(x)
-    x = self.output(x)
-
-    # raise NotImplementedError("Forward pass of the Q-network is missing.")
-    return x
+    def forward(self, x):
+        # Implement this
+        action_probs = self.model(x)
+        return action_probs
+        # raise NotImplementedError("Forward pass of the Q-network is missing.")
 
 class SARSA:
-  def __init__(self, state_dim, action_dim, gamma):
-    self._q = Q(state_dim, action_dim)
-    # Untoggle this if you have CUDA supported GPU
-    # self._q.cuda()
-    self._gamma = gamma
-    self._loss_function = nn.MSELoss()
-    self._q_optimizer = optim.Adam(self._q.parameters(), lr=0.0005)
-    self._action_dim = action_dim
+    def __init__(self, state_dim, action_dim, gamma):
+        self._q = Q(state_dim, action_dim)
+        # Untoggle this if you have CUDA supported GPU
+        # self._q.cuda()
+        self._gamma = gamma
+        self._loss_function = nn.MSELoss()
+        self._q_optimizer = optim.Adam(self._q.parameters(), lr=0.0005)
+        self._action_dim = action_dim
 
-  def get_action(self, x, epsilon):
-    """
-    Epsilon-greedy policy
-    """
-    u = np.argmax(self._q(tt(x)).cpu().detach().numpy())
-    r = np.random.uniform()
-    if r < epsilon:
-      return np.random.randint(self._action_dim)
-    return u
+    def get_action(self, x, epsilon):
+        """
+        Epsilon-greedy policy
+        """
+        u = np.argmax(self._q(tt(x)).cpu().detach().numpy())
+        r = np.random.uniform()
+        if r < epsilon:
+          return np.random.randint(self._action_dim)
+        return u
 
-  def train(self, episodes, time_steps, epsilon):
-
-    stats = EpisodeStats(episode_lengths=np.zeros(episodes), episode_rewards=np.zeros(episodes))
-
-    for e in range(episodes):
-      print("%s/%s"%(e+1, episodes))
-      s = env.reset()
-      curr_action = sarsa.get_action(s, epsilon)
-      for t in range(time_steps):
-        ns, r, d, _ = env.step(curr_action)
-
-        stats.episode_rewards[e] += r
-        stats.episode_lengths[e] = t
-
-        # Implement this
-        # q_esti = self._q(tt(s).cpu().detach().numpy())
-        q_esti = self._q(tt(s))
-        q_target = q_esti.clone()
-
+    def update_weights(self, input, target):
         self._q_optimizer.zero_grad()
-
-        if (d):
-            q_target[curr_action] = r
-            loss_q_val = self._loss_function(q_esti, q_target)
-            loss_q_val.backward()
-            self._q_optimizer.step()
-            break
-
-        # get the Q values for next state: ns
-        q_estim_next = self._q(tt(ns))
-        next_action = self.get_action(ns, epsilon)
-        q_target[curr_action] = r + self._gamma * q_estim_next[next_action]
-
-        loss_q_val = self._loss_function(q_esti, q_target)
-        loss_q_val.backward()
+        output = self._loss_function(input, target)
+        output.backward()
         self._q_optimizer.step()
 
-        s = ns
-        a = next_action
-        # raise NotImplementedError("SARSA implementation is missing")
+    def train(self, episodes, time_steps, epsilon):
+        stats = EpisodeStats(episode_lengths=np.zeros(episodes), episode_rewards=np.zeros(episodes))
+        for e in range(episodes):
+            # print(f"episode: {e} of {episodes}")
+            s = env.reset()
+            a = self.get_action(s, epsilon)
+            for t in range(time_steps):
+                ns, r, d, _ = env.step(a)
 
-    return stats
+                stats.episode_rewards[e] += r
+                stats.episode_lengths[e] = t
+                # Implement this
+                current = self._q(tt(s))[a]
+                next_action = self.get_action(ns, epsilon)
+                target = r + (1-d)*self._gamma*self._q(tt(ns))[next_action]
+                self.update_weights(input=current, target=target.detach())
+
+                if d:
+                    break
+
+                s = ns
+                a = next_action
+            print(f"episode: {e}, reward: {stats.episode_rewards[e]} len: {stats.episode_lengths[e]}")
+        return stats
 
 def plot_episode_stats(stats, smoothing_window=10, noshow=False):
   # Plot the episode length over time
@@ -130,24 +120,24 @@ def plot_episode_stats(stats, smoothing_window=10, noshow=False):
       plt.show(fig2)
 
 if __name__ == "__main__":
-  env = MountainCarEnv()
-  state_dim = env.observation_space.shape[0]
-  action_dim = env.action_space.n
-  sarsa = SARSA(state_dim, action_dim, gamma=0.99)
+    env = MountainCarEnv()
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    sarsa = SARSA(state_dim, action_dim, gamma=0.99)
 
-  episodes = 1000
-  time_steps = 200
-  epsilon = 0.2
+    episodes = 1000
+    time_steps = 200
+    epsilon = 0.2
 
-  stats = sarsa.train(episodes, time_steps, epsilon)
+    stats = sarsa.train(episodes, time_steps, epsilon)
 
-  plot_episode_stats(stats)
+    plot_episode_stats(stats)
 
-  for _ in range(5):
-    s = env.reset()
-    for _ in range(200):
-      env.render()
-      a = sarsa.get_action(s, epsilon)
-      s, _, d, _ = env.step(a)
-      if d:
-        break
+    for _ in range(5):
+        s = env.reset()
+        for _ in range(200):
+            env.render()
+            a = sarsa.get_action(s, epsilon)
+            s, _, d, _ = env.step(a)
+            if d:
+                break
